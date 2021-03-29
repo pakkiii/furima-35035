7c79a5e8fd3f5007cd6cef523b08e1e96d538350
pakkiii <80090340+pakkiii@users.noreply.github.com>
2021-03-29 15:57:23 +0900

    fourth commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:id])
-    redirect_to action: :index unless current_user.id == @item.user.id
+    unless current_user.id == @item.user.id
+      redirect_to action: :index 
+    end
   end
 
   def easy
